4cb6264bf5312787cd3b81c9aa4cc847c63f8fdb
Leonardo de A. Lima <leonardo@asl-sl.com.br>
2021-02-04 13:20:36 -0300

    nova vsersão com documentação com script para execução

--- instVirt/__init__.py
+++ instVirt/__init__.py
@@ -1,1 +1,4 @@
-__version__="0.1dev0"
+'''Este aplicativo constrói a interface para auxílio em sala de aula.
+Desenvolvido dentro da filosofia de software livre.
+Academia do softwre livre'''
+__version__="0.1dev1"
